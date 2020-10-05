c71e69f3d3b9446e05ddec07c92a02612206fbcf
Willian Duarte <willian@dzigual.com.br>
2020-10-05 11:30:23 -0300

    Modificado o tamanho da tela.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,6 @@
 var capture;
-
 var song;
 var visao = 0;
-
 function preload() {
  song = loadSound('data/battle-magic.mp3');
 }
@@ -19,50 +17,50 @@ function draw() {
      case 1:       
          //print("Vision: 1");
          tint(255, 0 , 0, 200);
-         image(capture, width/2 - 300, height/2 - 200, width/2, height/2 + 50);   
+         image(capture, 0, 0, width, height);   
          break;
      case 2:
          //print("Vision: 2 ");
          tint(0, 255 , 0, 200);
-         image(capture, width/2 - 300, height/2 - 200, width/2, height/2 + 50);        
+         image(capture, 0, 0, width, height);      
          break;
      case 3:
          //print("Vision: 3");         
          tint(0, 0 , 255, 200);
-         image(capture, width/2 - 300, height/2 - 200, width/2, height/2 + 50);       
+         image(capture, 0, 0, width, height);         
          break;
      case 4:
          //print("Vision: 4");
-         image(capture, width/2 - 300, height/2 - 200, width/2, height/2 + 50); 
+         image(capture, 0, 0, width, height);  
          filtersInvert();
          break;
      case 5:
          //print("Vision: 5");
-         image(capture, width/2 - 300, height/2 - 200, width/2, height/2 + 50); 
+         image(capture, 0, 0, width, height);   
          filtersPosterize();
          break;
      case 6:
         //print("Vision: 6");
-        image(capture, width/2 - 300, height/2 - 200, width/2, height/2 + 50); 
+        image(capture, 0, 0, width, height);  
          filtersDilate();
          break;
      case 7:
          //print("Vision: 7");
-         image(capture, width/2 - 300, height/2 - 200, width/2, height/2 + 50); 
+         image(capture, 0, 0, width, height);  
          filtersBlur();
          break;
      case 8: 
          //print("Vision: 8");
-         image(capture, width/2 - 300, height/2 - 200, width/2, height/2 + 50); 
+         image(capture, 0, 0, width, height);  
          filtersErode();
          break;
      case 9: 
          //print("Vision: 9");
-         image(capture, width/2 - 300, height/2 - 200, width/2, height/2 + 50); 
+         image(capture, 0, 0, width, height);  
          filterTherehold();
          break;
     default: 
-         image(capture, width/2 - 300, height/2 - 200, width/2, height/2 + 50); 
+         image(capture, 0, 0, width, height);  
          visao = 0;
          break;
    }
